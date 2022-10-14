89e24c1d5d847561d1c3182c29635a6f09542bc0
jbherndon <herndonja3@gmail.com>
2022-10-14 17:17:46 +0000

    update 10/14

--- receipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ receipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe []= [
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com'+
+  '/photos/61b775620fb3fcc4cbf036c1/1:1/w_1920,c_limit/20211208%20Spaghetti%20Squash%20'+
+  'with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg')
+];
   constructor() { }
 
   ngOnInit(): void {
